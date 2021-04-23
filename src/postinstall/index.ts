17acf31e86e6b9609a11278c20d6574147665aa9
Laura Vuorenoja <laura.vuorenoja@op.fi>
2021-04-23 12:23:16 +0300

    Use new downloader in postinstall script.

--- src/postinstall/index.ts
+++ src/postinstall/index.ts
@@ -1,38 +1,11 @@
-import fs from 'fs';
-import log from '../log';
 import supported from './supported';
+import download from './download';
+import log from '../log';
 
-const binName = 'findy-agent-cli';
-const targetPath = './bin';
+async () => {
+  const platform = supported.platform[process.platform];
+  const arch = supported.arch[process.arch];
 
-const exec = (cmd: string) => {
-  const exec = require('child_process').exec;
-  return new Promise((resolve, reject) => {
-    exec(
-      cmd,
-      (error: Error, stdout: string | Buffer, stderr: string | Buffer) => {
-        if (error) {
-          console.warn(error);
-        }
-        resolve(stdout ? stdout : stderr);
-      }
-    );
-  });
+  const fileName = await download(platform, arch);
+  log.info(`Downloaded CLI binary to ${fileName}`)
 };
-
-const run = async (targetPath: string) => {
-  const { platform, arch } = process;
-  if (!(platform in supported.platform) || !(arch in supported.arch)) {
-    console.error(`Not supported: ${platform}, ${arch}`);
-    process.exit(1);
-  }
-  const path = `./dist/${binName}_${supported.platform[platform]}_${supported.arch[arch]}/${binName}`;
-  await exec(`cp ${path} ${targetPath}/${binName}`);
-};
-
-log.info(`Creating target path ${targetPath}`);
-if (!fs.existsSync(targetPath)) {
-  fs.mkdirSync(targetPath);
-}
-
-run(targetPath);
