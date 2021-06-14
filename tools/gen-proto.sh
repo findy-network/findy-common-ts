235fc7d6712be759b002a341a3765fa0349fe5e3
Laura Vuorenoja <laura.vuorenoja@op.fi>
2021-06-14 13:07:44 +0300

    Add clogs to code generation script.

--- tools/gen-proto.sh
+++ tools/gen-proto.sh
@@ -11,15 +11,21 @@ SRC_DIR=$CURRENT_DIR/../src/idl
 mkdir -p $PROTO_DIR
 rm $PROTO_DIR/* || true
 
+echo "Download proto files..."
+
 fetch-github-release findy-network findy-agent-api ./idl
 
 TARGET_DIR=$ABS_PROTO_DIR
+
+echo "Run JS generator..."
 
 grpc_tools_node_protoc \
     --js_out=import_style=commonjs,binary:$TARGET_DIR \
     --grpc_out=grpc_js:$TARGET_DIR \
     -I $TARGET_DIR \
     "$TARGET_DIR"agent.proto "$TARGET_DIR"protocol.proto
+
+echo "Run TS generator..."
 
 protoc \
     --plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts \
