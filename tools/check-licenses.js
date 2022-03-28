dcceb6607f46711b144f4d40f969f86f8277f82a
Laura Vuorenoja <laura.vuorenoja@op.fi>
2022-03-28 11:57:40 +0300

    Update package-lock-file.

--- tools/check-licenses.js
+++ tools/check-licenses.js
@@ -1,5 +1,7 @@
-import jsgl = require('js-green-licenses');
-import fs = require('fs');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const jsgl = require('js-green-licenses');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const fs = require('fs');
 const { mkdirSync, existsSync, writeFileSync, copyFileSync } = fs;
 
 // TODO: move to own module
