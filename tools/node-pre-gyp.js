7610d6dc41f01456d4cc9794464cb104225ebcd1
Laura Vuorenoja <laura.vuorenoja@gmail.com>
2023-03-16 21:48:30 +0200

    Use node-pre-gyp for binary download.

---
+++ tools/node-pre-gyp.js
@@ -0,0 +1,17 @@
+const os = require('os')
+
+const mode = process.argv[2]
+
+const target = mode === "arch" ? {
+  arm64: 'arm64',
+  ia32: 'i386',
+  x64: 'x86_64',
+}[os.arch()] : {
+  darwin: 'Darwin',
+  linux: 'Linux',
+  win32: 'Windows'
+}[os.platform()]
+
+if (target) {
+  console.log(target)
+}
